35f09282580372e69fe487e79c2b5dd3af01a4b8
tlott99 <travis.lott@svu.edu>
2023-02-01 21:00:06 -0500

    Updated to Shape Guessing System

--- expert.py
+++ expert.py
@@ -10,8 +10,30 @@ class Expert:
             self.dialogue = json.load(d_file)
 
     def start(self):
-        pass
+        keep_going = True
+        has_contributed = False
+        print(self.dialogue["intro"])
+        while keep_going:
+            r1 = input(f'{self.dialogue["q1"]}\n').lower()
+            r2 = input(f'{self.dialogue["q2"]}\n')
+            fruit_key = f"{r1}-{r2}"
+            fruit_value = self.kb.get(fruit_key)
+            if fruit_value: 
+                print(f'\n{self.dialogue["conclusion"]} {fruit_value.get("name")}.\n')
+            else:
+                new_fruit = input(f'\n{self.dialogue["not_found"]}\n').lower()
+                self.kb[fruit_key] = {"name": new_fruit}
+                with open(self.kb_path, 'w') as kb_file:
+                    json.dump(self.kb, kb_file)
+                has_contributed = True
 
+            
+
+
+
+            keep_going = input(f'{self.dialogue["repeat"]}\n').upper()[0] == "Y"
+            if has_contributed:
+                print(self.dialogue["contribution"])
 
 def main():
     pass
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from expert import Expert
 
 def main():
     print("Testing, I'm in main.")
-    e1 = Expert('data/kb_fruit.json','data/dialogue_fruit.json')
+    e1 = Expert('data/kb_new.json','data/dialogue_new.json')
     e1.start()
 
 if __name__ == "__main__":
